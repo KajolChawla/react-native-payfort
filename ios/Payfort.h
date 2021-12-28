02c8bed19b58529966b9fd14da21e02bf1e97849
Kajol Chawla <kajol.c@sankeysolutions.com>
2021-12-28 12:28:29 +0530

    init

---
+++ ios/Payfort.h
@@ -0,0 +1,13 @@
+//
+//  Payfort.h
+//  Payfort
+//
+//  Created by Apple on 27/12/21.
+//  Copyright © 2021 Facebook. All rights reserved.
+//
+
+#ifndef Payfort_h
+#define Payfort_h
+
+
+#endif /* Payfort_h */
--- ios/Payfort.m
+++ ios/Payfort.m
@@ -1,6 +1,6 @@
 #import <React/RCTBridgeModule.h>
 
-@interface RCT_EXTERN_MODULE(Payfort, NSObject)
+@interface RCT_EXTERN_MODULE(MainViewController, NSObject)
 
 RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
                  withResolver:(RCTPromiseResolveBlock)resolve
